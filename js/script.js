1a6e9361110e52d598202a17cd0c88fef8528559
Gioele <gioele.armiento@gmail.com>
2021-03-23 16:53:39 +0100

    Dropdowns works on click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-function init() {
-    console.log($);
+
+function showDropdowns() {
+    var hasDropdown = $("li.has-dropdown, li.has-big-dropdown");
+    
+    hideDropdowns(hasDropdown);
+    $(this).find(".dropdown, .big-dropdown").addClass("active");
 }
+
+function hideDropdowns(hasDropdown) {
+
+    hasDropdown.each(function() {
+
+        hasDropdown.children().removeClass("active");
+    })
+}
+
+
+function init() {
+    
+    var hasDropdown = $("li.has-dropdown, li.has-big-dropdown");
+    console.log(hasDropdown);
+    hasDropdown.click(showDropdowns);
+    
+}
+
 $(document).ready(init);
