c5265ad4ddf66f8119066db0a4ebb204ef42ce39
Gioele <gioele.armiento@gmail.com>
2021-03-23 17:12:20 +0100

    Still working

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function showDropdowns() {
     var hasDropdown = $("li.has-dropdown, li.has-big-dropdown");
     
     hideDropdowns(hasDropdown);
+
     $(this).find(".dropdown, .big-dropdown").addClass("active");
 }
 
@@ -10,8 +11,11 @@ function hideDropdowns(hasDropdown) {
 
     hasDropdown.each(function() {
 
-        hasDropdown.children().removeClass("active");
-    })
+        if (hasDropdown.children().hasClass("active")) {
+
+            hasDropdown.children().removeClass("active")
+        }
+    });
 }
 
 
